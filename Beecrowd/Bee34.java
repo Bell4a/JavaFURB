da203a23fde4cc17de7d52537583fd64a7b1232e
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-05-25 21:52:34 -0300

    Update and rename Bee34.java to Beecrowd/Bee34.java

---
+++ Beecrowd/Bee34.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bee34 {
+
+    public static void main(String[] args) {
+        //variaveis
+        Double A, B, MEDIA;
+
+        //classe pra leitura dos dados
+        Scanner teclado = new Scanner (System.in);
+
+        A = teclado.nextDouble();
+        B = teclado.nextDouble();
+
+        //somar os valores
+        MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+        //resultado
+        System.out.printf ("MEDIA = %.5f\n", MEDIA);
+
+        teclado.close();
+
+    }
+    
+}
