1e2c37d7763f0c0fd3f85ce0c7d66d510b9ac9fb
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-14 21:49:13 -0300

    Enhance Carro class with new attributes and methods
    Added year, state, and distance attributes with methods to start and stop the car.

--- POO/models/Carro.java
+++ POO/models/Carro.java
@@ -3,4 +3,24 @@ public class Carro {
     String cor;
     Pessoa dono;
     Motor motor;
+    int ano;
+    boolean ligado;
+    int quantKm;
+
+    public boolean ligarCarro() {
+        if (ligado == false) {
+            ligado = true;
+            return ligado;
+        }
+        return ligado;
+    }
+
+    public boolean desligarCarro() {
+        if (ligado == true) {
+            ligado = false;
+            return ligado;
+        }
+        return ligado;
+    }
 }
+
