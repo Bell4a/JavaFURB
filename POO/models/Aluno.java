2dd2814fa403b855bd7bc8d04ce0bf490ddf2828
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-14 21:50:18 -0300

    Update Aluno.java

--- POO/models/Aluno.java
+++ POO/models/Aluno.java
@@ -4,4 +4,15 @@ public class Aluno {
     int matricula;
     double nota;
     boolean aprovado;
+
+    public double retornarNota() {
+        return nota;
+    }
+
+    public boolean analisarNota() {
+        if (nota >= 7) {
+            return true;
+        }
+        return false;
+    }
 }
