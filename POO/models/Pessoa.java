e817ee38b61c34f139c60f5fc0eba79d808a093e
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-14 21:49:51 -0300

    Enhance Pessoa class with getter and utility methods
    Added methods for accessing and modifying Pessoa attributes.

--- POO/models/Pessoa.java
+++ POO/models/Pessoa.java
@@ -6,4 +6,39 @@ public class Pessoa {
     int idade;
     double altura;
     boolean maiorIdade;
+
+    public String obterNome() {
+        return nome;
+    }
+
+    public int obterIdade() {
+        return idade;
+    }
+
+    public double obterAltura() {
+        return altura;
+    }
+
+    public boolean retornarMaiorIdade() {
+        if(idade >= 18) {
+            return true;
+        }
+        return false;
+    }
+
+    public String retornarMensagem() {
+        return (nome + "possui" + idade + "anos");
+    }
+
+    public int acrescentarIdade() {
+        int soma = idade + 1;
+        return soma;
+    }
+ 
+    public void inserirNome(String nome) {
+        if (nome.contains(s: "01")) {
+            System.out.println("Nome inválido");
+        }
+        this.nome = nome;
+    }
 }
