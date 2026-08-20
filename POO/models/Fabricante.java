439f8670516ba1b559baa222f94a2ce499706f41
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:35:00 -0300

    Refactor Fabricante class to use encapsulation
    Encapsulated fields 'nome' and 'pais' with private access and added getter and setter methods.

--- POO/models/Fabricante.java
+++ POO/models/Fabricante.java
@@ -1,4 +1,17 @@
 public class Fabricante {
-    String nome;
-    String pais;
+    private String nome;
+    private String pais;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
