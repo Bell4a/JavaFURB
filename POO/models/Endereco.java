9a9afa5c7369a56872bae1209e9f7c3c84917ffe
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:34:45 -0300

    Refactor Endereco class for encapsulation
    Refactored Endereco class to use private fields and added getter and setter methods for each field.

--- POO/models/Endereco.java
+++ POO/models/Endereco.java
@@ -1,9 +1,52 @@
 public class Endereco {
-    String rua;
-    String cidade;
-    String bairro;
-    String estado;
-    String pais;
-    int numero;
-    int cep;
+    private String rua;
+    private String cidade;
+    private String bairro;
+    private String estado;
+    private String pais;
+    private int numero;
+    private int cep;
+
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public int getCep() {
+        return cep;
+    }
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
 }
