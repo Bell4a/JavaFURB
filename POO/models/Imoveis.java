944317e1383fe1140f13b1b0b690cdc8be16c309
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:36:01 -0300

    Refactor Imoveis class for encapsulation
    Refactored Imoveis class to include private fields and getter/setter methods.

--- POO/models/Imoveis.java
+++ POO/models/Imoveis.java
@@ -1,6 +1,31 @@
 public class Imoveis {
-    String tipo;
-    double valor;
-    double area;
-    Pessoa proprietario;
+    private String tipo;
+    private double valor;
+    private double area;
+    private Pessoa proprietario;
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public double getArea() {
+        return area;
+    }
+    public void setArea(double area) {
+        this.area = area;
+    }
+    public Pessoa getProprietario() {
+        return proprietario;
+    }
+    public void setProprietario(Pessoa proprietario) {
+        this.proprietario = proprietario;
+    }
 }
