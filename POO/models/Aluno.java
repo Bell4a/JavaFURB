22f8e397303d975efa2cb5f09ae88713aea0eedf
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:33:04 -0300

    Refactor Aluno class to include accessors and mutators
    Added getter and setter methods for properties in Aluno class.

--- POO/models/Aluno.java
+++ POO/models/Aluno.java
@@ -1,9 +1,9 @@
 public class Aluno {
 
-    Pessoa[] pessoas;
-    int matricula;
-    double nota;
-    boolean aprovado;
+    private Pessoa[] pessoas;
+    private int matricula;
+    private double nota;
+    private boolean aprovado;
 
     public double retornarNota() {
         return nota;
@@ -14,5 +14,37 @@ public class Aluno {
             return true;
         }
         return false;
+    }
+
+    public Pessoa[] getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(Pessoa[] pessoas) {
+        this.pessoas = pessoas;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
     }
 }
