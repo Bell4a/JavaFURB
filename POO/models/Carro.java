36d596574601a9ec299ac6de4adfe1ab7f6af2ae
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:33:33 -0300

    Refactor Carro class with encapsulation and accessors
    Refactored Carro class to use private fields and added getter/setter methods for all attributes.

--- POO/models/Carro.java
+++ POO/models/Carro.java
@@ -1,11 +1,11 @@
 public class Carro {
-    String modelo;
-    String cor;
-    Pessoa dono;
-    Motor motor;
-    int ano;
-    boolean ligado;
-    int quantKm;
+    private String modelo;
+    private String cor;
+    private Pessoa dono;
+    private Motor motor;
+    private int ano;
+    private boolean ligado;
+    private int quantKm;
 
     public boolean ligarCarro() {
         if (ligado == false) {
@@ -21,6 +21,62 @@ public class Carro {
             return ligado;
         }
         return ligado;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Pessoa getDono() {
+        return dono;
+    }
+
+    public void setDono(Pessoa dono) {
+        this.dono = dono;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public int getQuantKm() {
+        return quantKm;
+    }
+
+    public void setQuantKm(int quantKm) {
+        this.quantKm = quantKm;
     }
 }
 
