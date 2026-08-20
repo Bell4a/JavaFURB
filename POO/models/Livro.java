f781ed583fe5d4f807ec487b320944fd99a1ab71
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:36:35 -0300

    Refactor Livro class for encapsulation and accessors
    Refactor Livro class to use private fields and add getter/setter methods.

--- POO/models/Livro.java
+++ POO/models/Livro.java
@@ -1,8 +1,8 @@
 public class Livro {
-    String titulo;
-    Pessoa autor;
-    int numeroPaginas;
-    int anoPublicacao;
+    private String titulo;
+    private Pessoa autor;
+    private int numeroPaginas;
+    private int anoPublicacao;
 
     public String retornarTitulo() {
         return titulo;
@@ -15,4 +15,37 @@ public class Livro {
     public String descrever() {
         return (titulo + "-" + numeroPaginas + "-" + anoPublicacao);
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Pessoa getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Pessoa autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
 }
