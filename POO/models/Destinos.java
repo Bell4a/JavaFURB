dee875a0af06902da3bbd8b9c48dcf61efa9e924
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:34:17 -0300

    Encapsulate fields in Destinos class with getters/setters
    Refactor Destinos class to encapsulate fields and add getters/setters.

--- POO/models/Destinos.java
+++ POO/models/Destinos.java
@@ -1,5 +1,24 @@
 public class Destinos {
-    String cidade;
-    String estado;
-    String pais;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
