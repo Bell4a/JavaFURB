da85af238be0496a6763f928239a8465fef848b2
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:37:52 -0300

    Refactor Produto class for encapsulation
    Refactored Produto class to use encapsulation with private fields and added getter and setter methods for each field.

--- POO/models/Produto.java
+++ POO/models/Produto.java
@@ -1,13 +1,47 @@
 public class Produto {
     
-    String nome;
-    Pessoa comprador;
-    double preco;
-    int quantEstoque;
-    boolean disponivel;
-    Fabricante fabricante;
+    private String nome;
+    private Pessoa comprador;
+    private double preco;
+    private int quantEstoque;
+    private boolean disponivel;
+    private Fabricante fabricante;
 
-    public double retornarPreco() {
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Pessoa getComprador() {
+        return comprador;
+    }
+    public void setComprador(Pessoa comprador) {
+        this.comprador = comprador;
+    }
+    public double getPreco() {
         return preco;
     }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantEstoque() {
+        return quantEstoque;
+    }
+    public void setQuantEstoque(int quantEstoque) {
+        this.quantEstoque = quantEstoque;
+    }
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+
 }
