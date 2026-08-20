d8061f204fb20311c1709e43bb3babed0fa675ae
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:35:50 -0300

    Update Imobiliaria.java

--- POO/models/Imobiliaria.java
+++ POO/models/Imobiliaria.java
@@ -1,5 +1,24 @@
 public class Imobiliaria {
-    Pessoa proprietario;
-    Endereco endereco;
-    Imoveis[] imoveis;
+    private Pessoa proprietario;
+    private Endereco endereco;
+    private Imoveis[] imoveis;
+
+    public Pessoa getProprietario() {
+        return proprietario;
+    }
+    public void setProprietario(Pessoa proprietario) {
+        this.proprietario = proprietario;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    public Imoveis[] getImoveis() {
+        return imoveis;
+    }
+    public void setImoveis(Imoveis[] imoveis) {
+        this.imoveis = imoveis;
+    }
 }
