cbe05652aaff61b74140ab16ba774bdade1987b5
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:35:16 -0300

    Refactor Funcionario class with getters and setters

--- POO/models/Funcionario.java
+++ POO/models/Funcionario.java
@@ -1,7 +1,32 @@
 public class Funcionario {
     
-    Pessoa nome;
-    String cargo;
-    double salario;
-    Departamento departamento;
+    private Pessoa nome;
+    private String cargo;
+    private double salario;
+    private Departamento departamento;
+
+    public Pessoa getNome() {
+        return nome;
+    }
+    public void setNome(Pessoa nome) {
+        this.nome = nome;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
 }
