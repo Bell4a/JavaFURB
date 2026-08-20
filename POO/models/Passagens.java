4269364b7b3840f3cde62c9bc9aad6a5f11cd681
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:37:05 -0300

    Encapsulate fields and add accessors in Passagens
    Refactor Passagens class to encapsulate fields and add getters/setters.

--- POO/models/Passagens.java
+++ POO/models/Passagens.java
@@ -1,5 +1,24 @@
 public class Passagens {
-    String codigo;
-    double valor;
-    boolean passagemConfirmada;
+    private String codigo;
+    private double valor;
+    private boolean passagemConfirmada;
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public boolean isPassagemConfirmada() {
+        return passagemConfirmada;
+    }
+    public void setPassagemConfirmada(boolean passagemConfirmada) {
+        this.passagemConfirmada = passagemConfirmada;
+    }
 }
