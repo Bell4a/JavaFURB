8a0e73e98388e8e72d1a52ad936d678034d816eb
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:36:16 -0300

    Refactor Item class to use encapsulation

--- POO/models/Item.java
+++ POO/models/Item.java
@@ -1,4 +1,17 @@
 public class Item {
-    String produto;
-    int quantidade;
+    private String produto;
+    private int quantidade;
+
+    public String getProduto() {
+        return produto;
+    }
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
