f616fa028a22bb9b2e46ac5ca8bf5a4d216938a7
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:37:19 -0300

    Refactor Pedido class with encapsulation and accessors

--- POO/models/Pedido.java
+++ POO/models/Pedido.java
@@ -1,4 +1,18 @@
 public class Pedido {
-    Pessoa pessoa;
-    Item pedido;
+    private Pessoa pessoa;
+    private Item pedido;
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+    public Item getPedido() {
+        return pedido;
+    }
+    public void setPedido(Item pedido) {
+        this.pedido = pedido;
+    }
+
 }
