8557f9a918e800b59aff4463f226b989a25a35af
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:34:03 -0300

    Refactor Departamento class to add getters and setters

--- POO/models/Departamento.java
+++ POO/models/Departamento.java
@@ -1,5 +1,24 @@
 public class Departamento {
-    String nome;
-    String sigla;
-    Funcionario funcionario;
+    private String nome;
+    private String sigla;
+    private Funcionario funcionario;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSigla() {
+        return sigla;
+    }
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
 }
