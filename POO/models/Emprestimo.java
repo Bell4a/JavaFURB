ad410c4ec4c614d8753d73046228455b778a3e1d
Bell4a <133617482+Bell4a@users.noreply.github.com>
2026-08-19 21:34:30 -0300

    Refactor Emprestimo class to use private fields

--- POO/models/Emprestimo.java
+++ POO/models/Emprestimo.java
@@ -1,4 +1,17 @@
 public class Emprestimo {
-    String dataEmprestimo;
-    String dataDevolucao;
+    private String dataEmprestimo;
+    private String dataDevolucao;
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
 }
